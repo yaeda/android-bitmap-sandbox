a644f00af1fa99af3ee600f85508d20c9ae57806
Takeshi YAEDA <t.yaeda@gmail.com>
2016-02-12 23:21:42 +0900

    Create empty activity project

---
+++ app/src/androidTest/java/com/kazeor/android/bitmapsandbox/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kazeor.android.bitmapsandbox;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
